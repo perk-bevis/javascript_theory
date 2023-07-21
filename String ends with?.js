175b39692f36c0b86568571e18cc2396f24add5a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-21 19:39:32 +0700

    String ends with?.js

---
+++ String ends with?.js
@@ -0,0 +1,22 @@
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+dịch:
+Hoàn thành giải pháp để nó trả về true nếu đối số (chuỗi) đầu tiên được truyền kết thúc bằng đối số thứ 2 (cũng là một chuỗi).
+
+Ví dụ:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+code:
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+console.log(solution('abc', 'bc'));  returns true
+console.log(solution('abc', 'd'));   returns false
