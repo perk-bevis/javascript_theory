c6e974144a5d40ed3f266c1efd0f9c4a3d40627a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-21 18:47:43 +0700

    Find Maximum and Minimum Values of a List.js

--- Find Maximum and Minimum Values of a List.js
+++ Find Maximum and Minimum Values of a List.js
@@ -8,6 +8,17 @@ Examples (Input -> Output)
 Notes
 You may consider that there will not be any empty arrays/vectors.
 
+dịch:
+Nhiệm vụ của bạn là tạo hai hàm ( maxvà min, hoặc maximumvà minimum, v.v., tùy thuộc vào ngôn ngữ ) nhận một danh sách các số nguyên làm đầu vào và trả về số lớn nhất và nhỏ nhất trong danh sách đó, tương ứng.
+
+Ví dụ (Đầu vào -> Đầu ra)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+ghi chú
+Bạn có thể cân nhắc rằng sẽ không có bất kỳ mảng/vectơ trống nào.
+
 code:
 function min(list) {
     let minValue = list[0];
