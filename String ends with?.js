d9a259b177c1e01064f13be8692be7d474438621
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-21 21:28:25 +0700

    String ends with?.js

--- String ends with?.js
+++ String ends with?.js
@@ -14,6 +14,7 @@ solution('abc', 'bc') // returns true
 solution('abc', 'd') // returns false
 
 code:
+Phương endsWith()thức xác định xem một chuỗi có kết thúc bằng các ký tự của một chuỗi đã chỉ định, trả về true hoặc false khi thích hợp.
 function solution(str, ending) {
   return str.endsWith(ending);
 }
