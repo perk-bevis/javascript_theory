bd9aaff362f99ae36bf4c27261b40e5b03aa4bab
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-23 07:08:59 +0700

    You're a square!.js

--- You're a square!.js
+++ You're a square!.js
@@ -17,6 +17,27 @@ Examples
  4  =>  true
 25  =>  true
 26  =>  false
+
+dịch:
+Một hình vuông của hình vuông
+Bạn thích xây dựng các khối. Bạn đặc biệt thích xây dựng các khối hình vuông. Và điều bạn thậm chí còn thích hơn, là sắp xếp chúng thành một khối hình vuông!
+
+Tuy nhiên, đôi khi, bạn không thể sắp xếp chúng thành một hình vuông. Thay vào đó, bạn sẽ có một hình chữ nhật bình thường! Những thứ bị nổ tung đó! Nếu bạn chỉ có một cách để biết liệu bạn có đang làm việc vô ích hay không… Đợi đã! Đó là nó! Bạn chỉ cần kiểm tra xem số khối xây dựng của bạn có phải là một hình vuông hoàn hảo hay không .
+
+Nhiệm vụ
+Cho một số nguyên, hãy xác định xem đó có phải là số chính phương không :
+
+Trong toán học, một số chính phương hoặc số chính phương là một số nguyên là bình phương của một số nguyên; nói cách khác, nó là tích của một số nguyên với chính nó.
+
+Các bài kiểm tra sẽ luôn sử dụng một số nguyên nào đó, vì vậy đừng lo lắng về điều đó trong các ngôn ngữ được gõ động.
+
+ví dụ
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
 code:
 
 var isSquare = function(n) {
