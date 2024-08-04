696ad70b34150b3542c4fbbdffa9032d5613f597
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-08-05 02:22:06 +0700

    Homework.js

--- Homework.js
+++ Homework.js
@@ -16,3 +16,39 @@ EX3: Đếm số lần xuất hiện của từng phần tử trong mảng:
 
 Bài toán: Cho một mảng các chuỗi hoặc số, hãy sử dụng reduce để đếm số lần xuất hiện của mỗi phần tử.
 Ví dụ: ['apple', 'banana', 'apple', 'orange', 'banana', 'banana'] sẽ cho kết quả { apple: 2, banana: 3, orange: 1 }.
+
+=====================================================================================================
+EX4:
+function findCallBackward(n, k) {
+    
+    let asD = true;
+
+    while (n > 1) {
+        const mod = Math.pow(2, n - 2);
+
+        if (k > mod) {
+            k -= mod; 
+            asD = !asD; 
+        }
+
+        n--; 
+    }
+
+    
+    return asD ? 'X' : 'Y';
+}
+const input = `4
+1 1
+2 1
+2 2
+3 4`;
+const columns = input.trim().split('\n');
+const T = parseInt(columns[0], 10);
+const results = [];
+
+for (let i = 1; i <= T; i++) {
+    const [n, k] = columns[i].split(' ').map(Number);
+    results.push(findCallBackward(n, k));
+}
+
+results.forEach(result => console.log(result));
