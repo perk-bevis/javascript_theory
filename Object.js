fb2f985699e482dc70b3c4621575ea9ae433c314
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-25 22:45:19 +0700

    Update Object.js

--- Object.js
+++ Object.js
@@ -87,3 +87,22 @@ console.log(calculate('multiply', 5, 3));
 console.log(calculate('divide', 9, 3));
 console.log(calculate('divide', 5,0));
 console.log(calculate('modulus', 5, 3));
+🔴🔴🔴🔴🔴
+function calculate (operation, a, b) {
+    const calculator = {
+        add: () => a + b,
+        subtract: () => a b,
+        multiply: () => a * b,
+        divide: () => b === 0 ? 'Cannot divide by zero.' : a/b
+    };
+    const action = calculator [operation];
+    return action? action() : 'Operation not recognized. Please use add, subtract, multiply, or divide.';
+}
+
+// Testing the function
+console.log(calculate('add', 5, 3));
+console.log(calculate('subtract', 5, 5));
+console.log(calculate('multiply', 5, 3));
+console.log(calculate('divide', 9, 3));
+console.log(calculate('divide', 5,0));
+console.log(calculate('modulus', 5, 3));
